ff9c2e56b06b9354f591b0cd91ed683003b4dd9f
sanjamkhera <88257752+sanjamkhera@users.noreply.github.com>
2024-04-23 06:11:19 -0700

    Initial Commit

---
+++ nodeapp/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// What you can get a response on main index route
+app.get('/', (req, res) => {
+  res.send('Service is up and running.');
+});
+
+app.listen(8080, () => {
+  console.log('Server is up and running on port 8080');
+});
